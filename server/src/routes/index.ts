34a50ef81b2949126a7af8e39c8bf523169dcc59
Robbe Coudenys <robbe.coudenys@gmail.com>
2022-02-13 11:09:50 +0100

    fix server tsconfig

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,7 +2,8 @@ import { Express, Request, Response } from 'express';
 const groupsRouter = require('./groups');
 
 export default function (app: Express) {
-    app.get('/', (req: Request, res: Response) => res.json({message: 'alive'}));
+    app.get('/', (req: Request, res: Response) => res.json({ message: 'alive' }));
     app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200));
+    app.get('/bartje', (req: Request, res: Response) => res.sendStatus(200));
     app.use('/groups', groupsRouter);
 }
